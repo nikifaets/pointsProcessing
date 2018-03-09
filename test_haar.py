c51c5b3422f3afb2ee4ef3826c6bcaec0a9082fd
Nikolay Pashov <nikifaets11@gmail.com>
2018-03-09 12:49:35 +0200

    sdf

--- test_haar.py
+++ test_haar.py
@@ -3,14 +3,22 @@ import numpy as np
 
 casc = cv2.CascadeClassifier("haar_new_s15/cascade.xml")
 
+cap = cv2.VideoCapture(1)
 path = "annotations1/scaled/laser32.jpg"
-img = cv2.imread(path)
+#img = cv2.imread(path)
 #img = cv2.cvtColor()
-points = casc.detectMultiScale(img)
 
-for(x, y, h, w) in points:
-        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+while(True):
 
-cv2.imshow("img", img)
-cv2.waitKey()
+	ret, img = cap.read()
+	points = casc.detectMultiScale(img)
+
+	for(x, y, h, w) in points:
+		cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+
+	cv2.imshow("img", img)
+	k = cv2.waitKey(1)
+	if k == ord('q'):
+		break
+
 
