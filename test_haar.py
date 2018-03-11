c7576326c4d131d08d4dbf70d0d8c47d9f9d8bb1
Nikolay Pashov <nikifaets11@gmail.com>
2018-03-12 00:52:15 +0200

    training

--- test_haar.py
+++ test_haar.py
@@ -1,11 +1,20 @@
 import cv2
 import numpy as np 
 
-casc = cv2.CascadeClassifier("haar_new_s15/cascade.xml")
+casc = cv2.CascadeClassifier("haar_new_s20/cascade.xml")
 
-cap = cv2.VideoCapture(1)
-path = "annotations1/scaled/laser32.jpg"
-#img = cv2.imread(path)
+cap = cv2.VideoCapture(0)
+'''path = "annotations/scaled/2017-11-09-214806.jpg"
+img = cv2.imread(path)
+points = casc.detectMultiScale(img)
+
+for(x, y, h, w) in points:
+	cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+
+cv2.imshow("img", img)
+cv2.waitKey()'''
+
+
 #img = cv2.cvtColor()
 
 while(True):
