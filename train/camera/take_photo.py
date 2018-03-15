6febb5f1dfc87cde6d5816e1cc8179a8521805d2
Nikolay Pashov <nikifaets11@gmail.com>
2018-03-15 20:51:31 +0200

    sdf

---
+++ train/camera/take_photo.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np  
+
+cam = -5
+ret = False
+
+while(not ret):
+
+	cap = cv2.VideoCapture(cam)
+	ret,img = cap.read()
+	print("trying ", ret)
+
+
+
+num = 40
+while(True):
+
+	ret, img = cap.read()
+
+	if ret:
+
+		cv2.imshow("img", img)
+
+		k = cv2.waitKey(1)
+		if k == ord("s"):
+			cv2.imwrite("sample" + str(num)+".jpg", img)
+			print("IMAGE TAKEN")
+			num+=1
+
+		if k == ord("q"):
+			break
