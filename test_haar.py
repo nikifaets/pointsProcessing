f82ecddc2348a4007ca071300b82cdb2001f35f6
Nikolay Pashov <nikifaets11@gmail.com>
2018-04-01 21:44:58 +0300

    dsf

--- test_haar.py
+++ test_haar.py
@@ -1,34 +1,30 @@
 import cv2
 import numpy as np 
 
-casc = cv2.CascadeClassifier("hog_s10/cascade.xml")
+casc = cv2.CascadeClassifier("src/haar_new_s10/cascade.xml")
 
 cap = cv2.VideoCapture(0)
 
 points = list()
 
-while(True):
 
-	
+img = cv2.imread("laser/laser34.jpg")
+#img = cv2.resize(img,(480,640))
 
-	ret, img = cap.read()
-	detected = np.zeros((480,640,1), np.uint8)
-	
-	img = cv2.blur(img,(3,3) )
+h,w,c = img.shape
+detected = np.zeros((h,w,1), np.uint8)
 
-	if not tracking:
-		points = casc.detectMultiScale(img)
+#img = cv2.blur(img,(3,3) )
 
-		for(x, y, h, w) in points:
-			cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
-			detected[int(y+h/2)][int(x+w/2)] = 255
+points = casc.detectMultiScale(img)
+
+for(x, y, h, w) in points:
+	cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0), 2)
+	detected[int(y+h/2)][int(x+w/2)] = 255
 
 
-	cv2.imshow("points", detected)
-	cv2.imshow("img", img)
-	k = cv2.waitKey(1)
-
-	if k == ord('q'):
-		break
+cv2.imshow("points", detected)
+cv2.imshow("img", img)
+k = cv2.waitKey(0)
 
 
